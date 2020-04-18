777bd6f82a79e5456e62c3aaf1dee4fc986a59d7
happysalada <raphael.megzari@gmail.com>
2020-04-18 19:09:05 +0900

    use grid for blog page layout

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -21,10 +21,26 @@
   <title>{post.title}</title>
 </svelte:head>
 
-<div class="container w-full mx-auto pt-20">
+<style>
+  .content-grid {
+    display: grid;
+    grid-template-columns: 15vw 70vw 15vw;
+    grid-template-rows: 10vh minmax(80vh, auto) 10vh;
+    grid-template-areas:
+      ". . ."
+      ". content ."
+      ". . .";
+  }
+
+  .content {
+    grid-area: content;
+  }
+</style>
+
+<div class="content-grid">
   <div
-    class="w-full px-4 md:px-6 text-xl leading-normal text-indigo-100"
-    style="font-family:Georgia,serif;"
+    class="content w-full text-xl leading-normal text-white"
+    style="font-family: Georgia, serif;"
   >
     <!--Title-->
     <div class="font-sans">
@@ -39,7 +55,7 @@
       <p class="text-sm font-normal text-gray-500">
         Tags: {#each post.tags as tag}
         <span
-          class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 bg-indigo-100 text-indigo-800"
+          class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 text-blue-500 border-2 border-blue-500"
         >
           {tag}
         </span>
