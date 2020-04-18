49b34b2c1268f42cfa1626dff72d1adf03437948
happysalada <raphael.megzari@gmail.com>
2020-04-18 17:49:52 +0900

    home style fixes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,24 @@
 <svelte:head>
   <title>Megzari.com</title>
 </svelte:head>
+<style>
+  .headline {
+    font-size: 2rem;
+  }
+
+  @media (min-width: 768px) {
+    .headline {
+      font-size: 5rem;
+    }
+  }
+</style>
 
 <div class="h-full flex flex-col justify-center text-center my-auto mx-auto">
-  <p class="mx-auto mb-3 text-base text-gray-400 sm:text-lg md:text-xl">
+  <p class="text-base mx-auto text-gray-400 md:text-xl">
     Hi! I'm Raphael
   </p>
   <h2
-    class="text-4xl tracking-tight leading-10 font-extrabold text-white sm:text-5xl sm:leading-none md:text-6xl"
+    class="headline tracking-tight leading-10 font-extrabold text-white mt-3 md:mt-10"
   >
     I
     <a
@@ -19,9 +30,7 @@
     <br class="xl:hidden" />
     ideas to life
   </h2>
-  <p
-    class="mt-3 max-w-md mx-auto text-base text-gray-400 sm:text-lg md:text-xl"
-  >
+  <p class="mt-3 md:mt-10 max-w-md mx-auto text-base text-gray-400 md:text-xl">
     welcome to my personal website.
   </p>
   <div class="mt-5 max-w-md mx-auto sm:flex sm:justify-center md:mt-8">
@@ -33,7 +42,7 @@
         Blog
       </a>
     </div>
-    <div class="ml-3 rounded-md shadow">
+    <div class="rounded-md shadow mt-3 md:ml-3 md:mt-0">
       <a
         href="/about"
         class="w-full flex items-center justify-center px-8 py-3 border-4 text-base leading-6 font-medium rounded-md text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-black focus:outline-none focus:shadow-outline-blue transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
