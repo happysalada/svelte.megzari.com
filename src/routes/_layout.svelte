f315a777e16bc56cb0cbe89cd64ad6208aae6205
happysalada <raphael.megzari@gmail.com>
2020-04-19 08:10:49 +0900

    fix menu on mobile

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,44 +1,46 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-	import Footer from '../components/Footer.svelte';
+  import Nav from '../components/Nav.svelte';
+  import Footer from '../components/Footer.svelte';
 
-	export let segment;
+  export let segment;
 </script>
 <style>
-	.grid {
-		display: grid;
-		grid-template-rows: 10vh minmax(80vh, auto) 10vh;
-		grid-template-areas: 
-			"nav"
-			"main"
-			"footer";
-	}
+  /* minmax on the nav is used for expansion on mobile */
+  /* minmax on the main is used for long main content */
+  .grid {
+    display: grid;
+    grid-template-rows: minmax(10vh, auto) minmax(80vh, auto) 10vh;
+    grid-template-areas: 
+      "nav"
+      "main"
+      "footer";
+  }
 
-	.nav {
-		grid-area: nav;
-	}
+  .nav {
+    grid-area: nav;
+  }
 
-	.main {
-		grid-area: main;
-	}
+  .main {
+    grid-area: main;
+  }
 
-	.footer {
-		grid-area: footer;
-	}
+  .footer {
+    grid-area: footer;
+  }
 </style>
 
 <div class="grid">
-	<div class="nav">
+  <div class="nav">
     <Nav {segment}/>
   </div>
 
-	<div class="main">
-		<main class="h-full">
-		  <slot></slot>
+  <div class="main">
+    <main class="h-full">
+      <slot></slot>
     </main>
   </div>
 
-	<div class="footer">
-		<Footer />
+  <div class="footer">
+    <Footer />
   </div>
 </div>
