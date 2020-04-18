bb25a00a53649b0bc491a8bdc5b91284f78631a1
happysalada <raphael.megzari@gmail.com>
2020-04-19 07:48:04 +0900

    add initial about me page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,49 @@
+<script>
+  const tags = ["Geek"];
+</script>
+
 <svelte:head>
-	<title>About</title>
+  <title>About Raphael Megzari</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<style>
+  .content-grid {
+    display: grid;
+    grid-template-columns: 15vw 70vw 15vw;
+    grid-template-rows: 10vh minmax(80vh, auto) 10vh;
+    grid-template-areas:
+      ". . ."
+      ". content ."
+      ". . .";
+  }
 
-<p>This is the 'about' page. There's not much here.</p>
+  .content {
+    grid-area: content;
+  }
+</style>
+
+<div class="content-grid">
+  <div
+    class="content w-full text-xl leading-normal text-white"
+    style="font-family: Georgia, serif;"
+  >
+    <!--Title-->
+    <div class="font-sans">
+      <h1
+        class="font-sans break-normal text-white pt-6 pb-2 text-3xl md:text-4xl"
+      >
+        About me
+      </h1>
+      <p class="text-sm font-normal text-gray-500">
+        Tags: {#each tags as tag}
+        <span
+          class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 text-blue-500 border-2 border-blue-500 mx-1 my-1"
+        >
+          {tag}
+        </span>
+        {/each}
+      </p>
+    </div>
+  </div>
+</div>
+<!--/container-->
