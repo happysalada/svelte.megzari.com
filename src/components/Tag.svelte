820a3bac6b5de4125015933ef254ca3f87f6cb8e
happysalada <raphael.megzari@gmail.com>
2020-04-19 11:51:53 +0900

    add links to tags

--- src/components/Tag.svelte
+++ src/components/Tag.svelte
@@ -1,8 +1,10 @@
 <script>
   export let tag;
 </script>
-<span
-  class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 text-blue-500 border-2 border-blue-500 mx-1 my-1"
->
-  { tag }
-</span>
+<a href="{ tag.link }" target="_blank">
+  <span
+    class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium leading-5 text-blue-500 border-2 border-blue-500 mx-1 my-1 hover:bg-blue-500 hover:text-black"
+  >
+    { tag.name }
+  </span>
+</a>
--- src/routes/_projects.js
+++ src/routes/_projects.js
@@ -2,13 +2,24 @@ const projects = [
   {
     title: "A high performance web crawler",
     description:
-      " The web crawler makes 100k req/day to various online travel agencies to gather price and availability data for hotel rooms",
+      "The web crawler makes 100k req/day to various online travel agencies to gather price and availability data for hotel rooms",
     client: {
       name: "MYSTAYS",
       url: "https://www.mystays.com/",
       caption: "a japanese hotel chain",
     },
-    tags: ["Elixir", "Web scraping", "Web crawling", "TCP connections"],
+    tags: [
+      { name: "Elixir", link: "https://elixir-lang.org/" },
+      {
+        name: "Web scraping",
+        link: "https://en.wikipedia.org/wiki/Web_scraping",
+      },
+      {
+        name: "Web crawling",
+        link: "https://en.wikipedia.org/wiki/Web_crawler",
+      },
+      { name: "TCP connections", link: "https://github.com/elixir-mint/mint" },
+    ],
   },
 ];
 
