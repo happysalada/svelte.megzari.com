bd2b3eef5337b43eaba22c56ef4770d03f340b78
happysalada <raphael@megzari.com>
2023-12-16 17:22:29 +0900

    simplify error handling

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,16 +1,14 @@
 import type { HandleServerError } from '@sveltejs/kit';
 import { extract_log_params } from '$lib/utils';
 
-export const handleError: HandleServerError = async ({ error, event }) => {
+export const handleError: HandleServerError = async ({ error, event, status, message }) => {
   const errorId = crypto.randomUUID();
 
   event.locals.error = error?.toString() || undefined;
   //@ts-ignore
   event.locals.errorStackTrace = error?.stack || undefined;
   event.locals.errorId = errorId;
-  let status = error?.toString()?.includes("Not found") ? 404 : 500;
   let req_params = extract_log_params({ status }, event);
-  const message = status == 404 ? 'Page not found' : 'An unexpected error occurred.';
   console.error({ ...req_params, message });
 
   return {
