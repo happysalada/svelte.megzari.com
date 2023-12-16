42b8d299e62a20cc8b0360a335ceb9f555d4f74b
happysalada <raphael@megzari.com>
2023-12-16 16:01:54 +0900

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit, type UserConfig } from '@sveltejs/kit/vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
-import nodePolyfills from 'vite-plugin-node-stdlib-browser';
-import { isoImport } from 'vite-plugin-iso-import';
+// import nodePolyfills from 'vite-plugin-node-stdlib-browser';
+// import { isoImport } from 'vite-plugin-iso-import';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-  plugins: [isoImport(), nodePolyfills(), sveltekit(), ViteImageOptimizer({})]
+const config: UserConfig = {
+  plugins: [
+    // isoImport(),
+    // nodePolyfills(),
+    sveltekit(),
+    ViteImageOptimizer({})]
 };
 
 export default config;
